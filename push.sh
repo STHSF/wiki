75cd987fc9ba9cc3686fbdecfa2d6f6c564bcaf4
STHSF <woailiyu@163.com>
2017-06-27 09:30:54 +0800

    2017-06-27-09

--- push.sh
+++ push.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo "提交到主分支"
 time=`date "+%Y-%m-%d-%H"`
-
 git add . --all
 git commit -am ${time}
 git push origin master
 
+echo "simiki编译"
 simiki g
 
+echo "提交到gh-pages分支"
 fab deploy
